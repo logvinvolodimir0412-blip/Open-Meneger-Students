d752c59cbe2f58bbabdd8261668a24c747a01b8d
Logvin Vova <logvinvolodimir0412@gmail.com>
2026-05-25 13:17:02 +0300

    додано головне меню

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <string>
 
+//basic struct
 struct Student {
   std::string name;
   unsigned int age;
-}
+};
 
 int main(void){
-  std::cout<<"\n\t\tOpen Meneger Students"<<std::endl;
+  //main menu
+  std::string menu[] = {
+    "1. List students",
+    "2. Add student",
+    "0. Exit"
+  };
+  //menu len 
+  size_t len = sizeof(menu)/
+  sizeof(menu[0]);
+  
+  //title
+  std::cout<<"\n\t\tOpen Meneger Students\n"<<std::endl;
+  
+  //printing menu
+  for(size_t i=0; i<len; ++i){
+    std::cout<<menu[i]<<std::endl;
+  }
   
   return 0;
 }
