409c0bbc5b92ff627097d56771dfc62a59aaea3d
Logvin Vova <logvinvolodimir0412@gmail.com>
2026-05-25 11:11:47 +0300

    додано базову структуру

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+
+struct Student {
+  std::string name;
+  unsigned int age;
+}
+
 int main(void){
+  std::cout<<"\n\t\tOpen Meneger Students"<<std::endl;
+  
   return 0;
 }
